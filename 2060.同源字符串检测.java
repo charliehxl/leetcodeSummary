7eb7f9103013fafb99606ce68d52f98311294b34
hexinlu01 <hexinlu01@baidu.com>
2022-03-14 11:56:18 +0800

    git commit

---
+++ 2060.同源字符串检测.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=2060 lang=java
+ *
+ * [2060] 同源字符串检测
+ */
+
+// @lc code=start
+class Solution {
+    public boolean possiblyEquals(String s1, String s2) {
+
+    }
+}
+// @lc code=end
+
