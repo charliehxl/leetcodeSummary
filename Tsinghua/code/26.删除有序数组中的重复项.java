dd5eb3aeddde36122dff161e753e479436b129b7
hexinlu01 <hexinlu01@baidu.com>
2022-04-17 18:52:38 +0800

    哦,明天又要上班...

--- Tsinghua/code/26.删除有序数组中的重复项.java
+++ Tsinghua/code/26.删除有序数组中的重复项.java
@@ -7,7 +7,26 @@
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+        int pre = 1;
+        for (int cur = 1; cur < nums.length; cur++){
+            if (nums[cur] != nums[pre - 1]){
+                nums[pre++] = nums[cur];
+            }
+        }
+        return pre;
+    }
 
+    // 80题：每个元素 最多出现两次 
+    public int keep2Duplicates(int[] nums,int k){
+        int pre = 0;
+        for (int cur = 0; cur < nums.length; cur++){
+            // pre 始终停在目标数组最后一位，cur 游走
+            if(pre < k || nums[cur] != nums[pre - k]){
+                nums[pre++] = nums[cur];
+            }
+        }
+        return pre;
     }
 }
 // @lc code=end
---
+++ Tsinghua/code/28.实现-str-str.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=java
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+class Solution {
+
+    // 2022年04月17日18:51:09 写不出来，下一次不知道写不写出来
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0){
+            return 0;
+        }
+
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+        int j = -1;
+        for(int i = 0; i<haystack.length();i++){
+            while(j>=0 && haystack.charAt(i) != needle.charAt(j+1)){
+                j = next[j];
+            }
+            if(haystack.charAt(i)==needle.charAt(j+1)){
+                j++;
+            }
+            if(j==needle.length()-1){
+                return (i-needle.length()+1);
+            }
+        }
+        return -1;
+    }
+
+    public void getNext(int[] next, String s){
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i<s.length(); i++){
+            while(j>=0 && s.charAt(i) != s.charAt(j+1)){
+                j=next[j];
+            }
+
+            if(s.charAt(i)==s.charAt(j+1)){
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+}
+// @lc code=end
+
